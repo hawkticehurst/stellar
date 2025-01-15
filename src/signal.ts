08385f88333e64af216eebcf37b4d653c63ac88d
Hawk Ticehurst <hawkticehurst@gmail.com>
2025-01-14 16:29:40 -0800

    Reimplement and improve bind feature

--- src/signal.ts
+++ src/signal.ts
@@ -2,82 +2,110 @@ import { Signal } from 'signal-polyfill';
 import { effect } from './utils/effect.js';
 import { coerce } from './utils/coerce.js';
 
+type Bound = {
+	text?: HTMLElement[];
+	value?: HTMLElement[];
+	checked?: HTMLInputElement[];
+	files?: HTMLInputElement[];
+	audio?: HTMLElement[];
+	video?: HTMLElement[];
+	img?: HTMLElement[];
+	details?: HTMLElement[];
+	this?: HTMLElement[];
+}
+
 export class SignalElement<T> extends HTMLElement {
 	signal: Signal.State<T> | Signal.Computed<T>;
-	isBound: boolean;
-	hasLocalStorage: boolean;
 	isHTML: boolean;
 	stateAttr: string | null;
-	targetId: string | null;
-	targetElem: SignalElement<T> | null;
-	boundElem: HTMLInputElement | HTMLButtonElement | HTMLOptionElement | HTMLMeterElement | HTMLProgressElement | null;
+	boundElements: Bound;
+	hasLocalStorage: boolean;
 	mutation: (state: T) => unknown;
 	cleanup: () => void;
 
 	constructor() {
 		super();
-		this.isBound = this.getAttribute('bind:value') === "";
-		this.hasLocalStorage = this.getAttribute('local') !== null;
+
 		this.isHTML = this.getAttribute('render') === 'html';
 		this.stateAttr = this.getAttribute('state');
 		this.mutation = (state) => state;
-		if (this.isBound) {
-			if (this.children.length !== 1) {
-				throw new Error('Signal must contain a single child element when using bind directive.');
+		this.hasLocalStorage = this.getAttribute('local') !== null;
+
+		// Check for elements bound to this signal
+		this.boundElements = getBoundElements(`#${this.id}`);
+
+		// NOTE: There is no event to track when the text content of an element changes
+		// therefore, we cannot bind to text content changes. Plus if you're binding the
+		// value of this signal to the text content of an element, you're effectively just
+		// reflecting the value of the signal to the element. The chances of that element's
+		// text content changing without the signal's value changing is slim to none.
+				
+		// Bind element value to this signal
+		if (this.boundElements.value) {
+			for (const bound of this.boundElements.value) {
+				if (!(bound instanceof HTMLInputElement)) {
+					// TODO: Fix this error message (i.e. typeof bound)
+					throw new Error(`Cannot bind value property to ${typeof bound}`)
+				}
+				bound.addEventListener('input', () => {
+					this.state = coerce(bound.value) as unknown as T;
+				});
 			}
-			this.boundElem = this.children[0] as HTMLInputElement | HTMLButtonElement | HTMLOptionElement | HTMLMeterElement | HTMLProgressElement;
-			this.boundElem.addEventListener('input', (e) => {
-				this.state = this.boundElem?.value as unknown as T;
-			});
-			this.signal = new Signal.State(this.boundElem.value as unknown as T);
-			this.cleanup = effect(() => this.#render());
+		}
+
+		let initial;
+		if (this.stateAttr) {
+			// Always prefer using the `state` attribute if it exists
+			initial = coerce(this.stateAttr);
+		} else if (this.isHTML) {
+			// Use HTML content if `isHTML` flag is set
+			initial = this.getHTML();
 		} else {
-			let initial;
-			if (this.stateAttr) {
-				// Always prefer using the `state` attribute if it exists
-				initial = coerce(this.stateAttr);
-			} else if (this.isHTML) {
-				// Use HTML content if `isHTML` flag is set
-				initial = this.getHTML();
-			} else {
-				// Default to initializing with text content
-				const content = this.textContent;
-				initial = coerce(content);
-			}
-			// Attempt to override initial value if local storage flag is set
-			if (this.hasLocalStorage) {
-				const item = this.getAttribute('local');
-				if (item) {
-					const value = localStorage.getItem(item);
-					if (value) {
-						initial = coerce(value);
-					}
+			// Default to initializing with text content
+			const content = this.textContent;
+			initial = coerce(content);
+		}
+
+		// Attempt to override initial value if local storage flag is set
+		if (this.hasLocalStorage) {
+			const item = this.getAttribute('local');
+			if (item) {
+				const value = localStorage.getItem(item);
+				if (value) {
+					initial = coerce(value);
 				}
 			}
-			// Initialize signal
-			this.signal = new Signal.State(initial);
-			this.cleanup = effect(() => this.#render());
 		}
+
+		// Initialize signal
+		this.signal = new Signal.State(initial);
+		this.cleanup = effect(() => this.#render());
 	}
 	connectedCallback() {
 		this.#render();
 	}
 	disconnectedCallback() {
+		// Cleanup effect
 		this.cleanup();
+		
+		// Unbind element values from this signal
+		if (this.boundElements.value) {
+			for (const bound of this.boundElements.value) {
+				if (bound instanceof HTMLInputElement) {
+					bound.removeEventListener('input', () => {
+						this.state = coerce(bound.value) as unknown as T;
+					});
+				}
+			}
+		}
 	}
 	#render() {
 		if (this.signal) {
-			if (this.isBound) {
-				if (this.boundElem) {
-					this.boundElem.value = `${this.signal.get()}`;
-				}
+			const value = this.mutation(this.signal.get());
+			if (this.isHTML) {
+				this.setHTMLUnsafe(`${value}`);
 			} else {
-				const value = this.mutation(this.signal.get());
-				if (this.isHTML) {
-					this.setHTMLUnsafe(`${value}`);
-				} else {
-					this.textContent = `${value}`;
-				}
+				this.textContent = `${value}`;
 			}
 		}
 	}
@@ -88,7 +116,11 @@ export class SignalElement<T> extends HTMLElement {
 		if (!(this.signal instanceof Signal.State)) {
 			throw new Error('Cannot set value on a computed signal.');
 		}
+
+		// Update the signal
 		this.signal.set(v);
+
+		// Update local storage if flag is set
 		if (this.hasLocalStorage) {
 			const item = this.getAttribute('local');
 			if (!item) {
@@ -100,12 +132,23 @@ export class SignalElement<T> extends HTMLElement {
 			}
 			localStorage.setItem(item, `${v}`);
 		}
+
+		// Update bound elements
+		if (this.boundElements.text) {
+			for (const bound of this.boundElements.text) {
+				bound.textContent = `${v}`;
+			}
+		}
+		if (this.boundElements.value) {
+			for (const bound of this.boundElements.value) {
+				if (bound instanceof HTMLInputElement) {
+					bound.value = `${v}`;
+				}
+			}
+		}
 	}
 	// Define a custom renderer for the state
 	set render(callback: (state: T) => unknown) {
-		if (this.isBound) {
-			throw new Error('Cannot set a custom renderer on a bound signal.');
-		}
 		this.mutation = callback;
 		this.#render();
 	}
@@ -121,4 +164,41 @@ export class SignalElement<T> extends HTMLElement {
 		this.signal = signal;
 		this.cleanup = effect(() => this.#render());
 	}
+}
+
+function getBoundElements(id: string) {
+	const bound: Bound = {};
+	let elements = document.querySelectorAll(`[bind="${id}"]`);
+	for (const element of elements) {
+		if (element instanceof HTMLElement) {
+			if (!bound.text) {
+				bound.text = [];
+			}
+			bound.text.push(element);
+		}
+	}
+	elements = document.querySelectorAll(`[bind\\:value="${id}"]`);
+	for (const element of elements) {
+		if (element instanceof HTMLInputElement) {
+			if (!bound.value) {
+				bound.value = [];
+			}
+			bound.value.push(element);
+		}
+	}
+	// Get bound checkboxes and radios
+	// elements = document.querySelectorAll(`input[type="checkbox"][bind\\:checked="${id}"], input[type="radio"][bind\\:checked="${id}"]`);
+	// for (const element of elements) {
+	// 	if (element instanceof HTMLInputElement) {
+	// 		bound.value.push(element);
+	// 	}
+	// }
+	// Get bound file inputs
+	// elements = document.querySelectorAll(`input[type="file"][bind\\:files="${id}"]`);
+	// for (const element of elements) {
+	// 	if (element instanceof HTMLInputElement) {
+	// 		bound.value.push(element);
+	// 	}
+	// }
+	return bound;
 }
